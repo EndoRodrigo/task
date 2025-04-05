084a1a6ac8cf9cfb7176425722ed6bafa4f4b0d7
EndoRodrigoRodriguez <endorodrigo19@gmail.com>
2025-04-04 21:25:13 -0500

    feat: implementacion del cliente para el sistema de tareas

--- src/main/java/com/endorodrigo/task/TaskApplication.java
+++ src/main/java/com/endorodrigo/task/TaskApplication.java
@@ -1,13 +1,15 @@
 package com.endorodrigo.task;
 
-import org.springframework.boot.SpringApplication;
+import com.endorodrigo.task.presentation.SistemaTareasFx;
+import javafx.application.Application;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TaskApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TaskApplication.class, args);
+		//SpringApplication.run(TaskApplication.class, args);
+		Application.launch(SistemaTareasFx.class, args);
 	}
 
 }
