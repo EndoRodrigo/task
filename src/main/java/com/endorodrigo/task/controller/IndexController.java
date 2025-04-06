67435cb3cea880c92aa08948135a29d31b8873ad
EndoRodrigoRodriguez <endorodrigo19@gmail.com>
2025-04-05 19:23:46 -0500

    feat: Implementacion de la funcionalidad de listas clientes

--- src/main/java/com/endorodrigo/task/controller/IndexController.java
+++ src/main/java/com/endorodrigo/task/controller/IndexController.java
@@ -1,6 +1,20 @@
 package com.endorodrigo.task.controller;
 
+import com.endorodrigo.task.model.Tarea;
+import com.endorodrigo.task.presentation.SistemaTareasFx;
+import com.endorodrigo.task.service.TareaServicio;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.net.URL;
@@ -8,8 +22,47 @@ import java.util.ResourceBundle;
 
 @Component
 public class IndexController implements Initializable {
+
+    private static final Logger log = LoggerFactory.getLogger(IndexController.class);
+
+    @Autowired
+    private TareaServicio tareaServicio;
+
+    @FXML
+    private TableView<Tarea> tareaTabla;
+
+    @FXML
+    private TableColumn<Tarea, Integer> idColumna;
+
+    @FXML
+    private TableColumn<Tarea, String> tareaColumna;
+
+    @FXML
+    private TableColumn<Tarea, String> responsableColumna;
+
+    @FXML
+    private TableColumn<Tarea, String> statusColumna;
+
+    private final ObservableList<Tarea> tareaList = FXCollections.observableArrayList();
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        tareaTabla.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        configurarColumna();
+        listarTarea();
+    }
 
+    private void listarTarea() {
+        log.info("Listando tareas");
+        tareaList.clear();
+        tareaList.addAll(tareaServicio.listarTarea());
+        tareaTabla.setItems(tareaList);
+    }
+
+    private void configurarColumna() {
+        idColumna.setCellValueFactory(new PropertyValueFactory<>("idTarea"));
+        tareaColumna.setCellValueFactory(new PropertyValueFactory<>("nombreTarea"));
+        responsableColumna.setCellValueFactory(new PropertyValueFactory<>("responsable"));
+        statusColumna.setCellValueFactory(new PropertyValueFactory<>("estatus"));
     }
 }
